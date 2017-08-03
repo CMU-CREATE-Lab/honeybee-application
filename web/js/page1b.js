1c190bd0fb4fa2126b9640cea169636d251f799b
mtasota <miketasota@gmail.com>
2017-08-03 14:18:01 -0400

    Set and display device info

--- web/js/page1b.js
+++ web/js/page1b.js
@@ -8,18 +8,56 @@ var Page1B = {
 
   initialize: function() {
     console.log("Page1B.initialize");
-    html_device_name = $("#device-name");
-    html_device_hardware = $("#device-hw");
-    html_device_firmware = $("#device-fw");
-    html_device_serial_number = $("#device-serial");
+    this.html_device_name = $("#device-name");
+    this.html_device_hardware = $("#device-hw");
+    this.html_device_firmware = $("#device-fw");
+    this.html_device_serial_number = $("#device-serial");
+
+    if (!App.honeybee_device) {
+      console.warn("Went to Page1B but does not have a honeybee device; returning to previous page.");
+      App.goToPage("page1a");
+      return;
+    }
+    if (!App.honeybee_device.hasDeviceInfo) {
+      this.requestDeviceInfo();
+    }
+    this.displayDeviceInfo();
   },
 
 
-  displayDeviceInfo: function(name, hw, fw, serial) {
-    html_device_name.text(name);
-    html_device_hardware.text(hw);
-    html_device_firmware.text(fw);
-    html_device_serial_number.text(serial);
-  }
+  requestDeviceInfo: function() {
+    // TODO callback to application
+  },
+
+
+  setDeviceInfo: function(name, hw, fw, serial) {
+    if (!App.honeybee_device) {
+      console.warn("called setDeviceInfo but does not have a honeybee device; returning to previous page.");
+      App.goToPage("page1a");
+      return;
+    }
+    // Merge the contents of two or more objects together into the first object. (http://api.jquery.com/jQuery.extend/)
+    $.extend(App.honeybee_device, {hasDeviceInfo: true, device_name: name, hardware_version: hw, firmware_version: fw, serial_number: serial})
+
+    Page1B.displayDeviceInfo();
+  },
+
+
+  displayDeviceInfo: function() {
+    if (!App.honeybee_device) {
+      console.warn("called displayDeviceInfo but does not have a honeybee device; returning to previous page.");
+      App.goToPage("page1a");
+      return;
+    }
+    var name = !(App.honeybee_device.device_name) ? "--" : App.honeybee_device.device_name;
+    var hw = !(App.honeybee_device.hardware_version) ? "--" : App.honeybee_device.hardware_version;
+    var fw = !(App.honeybee_device.firmware_version) ? "--" : App.honeybee_device.firmware_version;
+    var serial = !(App.honeybee_device.serial_number) ? "--" : App.honeybee_device.serial_number;
+
+    this.html_device_name.text(name);
+    this.html_device_hardware.text(hw);
+    this.html_device_firmware.text(fw);
+    this.html_device_serial_number.text(serial);
+  },
 
 }
