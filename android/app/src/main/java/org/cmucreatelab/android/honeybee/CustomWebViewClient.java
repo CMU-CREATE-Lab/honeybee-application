aff2677b92dce789cd8e4d28548db2bb573c153c
mtasota <miketasota@gmail.com>
2017-08-09 14:54:06 -0400

    Add webview, remove action bar, and template code from previous example

---
+++ android/app/src/main/java/org/cmucreatelab/android/honeybee/CustomWebViewClient.java
@@ -0,0 +1,34 @@
+package org.cmucreatelab.android.honeybee;
+
+import android.net.Uri;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by mike on 8/2/17.
+ */
+
+public class CustomWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        Uri uri = Uri.parse(url);
+
+        // if request is "schema://" then we override
+        if (uri.getScheme().equals("schema")) {
+            Log.i("CustomWebViewClient", "Caught message from browser: host=" + uri.getHost()+"\nPath=" + uri.getPath());
+            view.loadUrl("javascript:alert(\"android\")");
+            return true;
+        }
+        // otherwise, do not override
+        return false;
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        Log.i("CustomWebViewClient", "Finished loading page URL="+url);
+    }
+
+}
--- android/app/src/main/java/org/cmucreatelab/android/honeybee/MainActivity.java
+++ android/app/src/main/java/org/cmucreatelab/android/honeybee/MainActivity.java
@@ -2,6 +2,8 @@ package org.cmucreatelab.android.honeybee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebChromeClient(new WebChromeClient());  // for alert messages
+        webView.setWebViewClient(new CustomWebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/web/index.html");
     }
 }
