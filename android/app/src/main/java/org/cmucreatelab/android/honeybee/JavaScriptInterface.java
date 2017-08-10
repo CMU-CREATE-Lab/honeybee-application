ccb2e1e24c1d84ccdc6ab5088d6a191857e7c3de
mtasota <miketasota@gmail.com>
2017-08-10 09:08:34 -0400

    android: helper for loading javascript into webview

--- android/app/src/main/java/org/cmucreatelab/android/honeybee/JavaScriptInterface.java
+++ android/app/src/main/java/org/cmucreatelab/android/honeybee/JavaScriptInterface.java
@@ -2,6 +2,7 @@ package org.cmucreatelab.android.honeybee;
 
 import android.bluetooth.BluetoothDevice;
 import android.net.Uri;
+import android.util.Log;
 import android.webkit.WebView;
 
 import java.util.ArrayList;
@@ -13,14 +14,21 @@ import java.util.ArrayList;
 public class JavaScriptInterface {
 
 
-    public static void notifyDeviceListChanged(final WebView view, ArrayList<BluetoothDevice> newList) {
+    public static void notifyDeviceListChanged(final WebView webView, ArrayList<BluetoothDevice> newList) {
         // TODO need to consider escape characters \" in device name and address
         String jsArray = "[";
         for (BluetoothDevice device: newList) {
             jsArray += "{name: \"" + device.getName() + "\", mac_address: \"" + device.getAddress() + "\"},";
         }
         jsArray += "]";
-        view.loadUrl("javascript:Page1A.notifyDeviceListChanged("+ Uri.encode(jsArray) +")");
+        sendJavaScript(webView, "Page1A.notifyDeviceListChanged("+ Uri.encode(jsArray) +")");
+    }
+
+
+    private static void sendJavaScript(final WebView webView, String script) {
+        String url = "javascript:" + script;
+        Log.v(MainActivity.LOG_TAG, "sendJavaScript: ``"+url+"``");
+        webView.loadUrl(url);
     }
 
 }
